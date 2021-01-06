dc34ee756551e88404bd483827e2bab032a850b6
Hugo <hugolachkar@gmail.com>
2021-01-06 20:05:51 +0100

    [UPDATE] clean portfolio

--- client/src/Components/Portfolio.js
+++ client/src/Components/Portfolio.js
@@ -7,7 +7,7 @@ class Portfolio extends Component {
       var title = this.props.data.title;
       var projects = this.props.data.projects.map(function(projects){
         var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
+        return <div key={projects.title} className="portfolio-item">
            <div className="item-wrap">
             <a href={projects.url} title={projects.title}>
                <img alt={projects.title} src={projectImage} />
@@ -17,7 +17,6 @@ class Portfolio extends Component {
                      <p>{projects.category}</p>
                   </div>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
             </a>
           </div>
         </div>
